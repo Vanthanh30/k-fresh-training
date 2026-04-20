2d74ae86b328175c900023a0cf60516bafa8448e
VinTent26 <155293495+VinTent26@users.noreply.github.com>
2026-04-20 10:12:29 +0700

    Revert "automation testcase"
    This reverts commit b914c90a9d8b9b3cdded898e906704b414317dca.

--- data/user.ts
+++
@@ -1,6 +0,0 @@
-import { User } from '../models/user';
-
-export const user: User = {
-  email: 'thanhho@gmail.com',
-  password: 'xX5KhJr!bvt6cCG',
-};
--- locators/wishlist.locators.ts
+++
@@ -1,8 +0,0 @@
-export const wishlistLocators = {
-  productCard: '.product-thumb',
-  addToWishlistBtn: 'button.btn-wishlist',
-  removeBtn: 'a.text-danger',
-  content: '#content',
-  wishlistTableRows: '#content table tbody tr',
-  productLink: 'a[href*="product_id"]', // ✅ Dùng href thay vì td.text-center
-};
